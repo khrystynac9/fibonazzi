545ef830365467a62650565b4e6e34917ac57160
Khrystyna <khrystynkakitty@gmail.com>
2016-11-09 19:49:42 +0000

    text

---
+++ js/index.js
@@ -0,0 +1,39 @@
+var n = Number(prompt("Задайте число n"));
+
+function fib(n) {
+    var f;
+    if (n >= 2) {
+        f = fib(n - 1) + fib(n - 2);
+    }
+    else {
+        f = n;
+    }
+    return "число Фібоначчі " + f;
+}
+var result = fib(n);
+document.write(result);
+
+
+
+
+//варіант другий =)
+
+//var m = fib(n);
+
+function fib(n) {
+    var x1 = 1,
+        x2 = 1;
+        var k;
+    var x3 = x1 + x2;
+    for (k = 3; k <= n; k++) {
+        x3 = x1 + x2;
+        x1 = x2;
+        x2 = x3;
+    };
+    return 'число Фібоначчі ' + x3;
+};
+
+document.write(fib(n));
+
+
+
